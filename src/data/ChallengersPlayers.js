32f18902f8f60841901a8d54b1993f0bec731a18
matty51299-a11y <matty51299@gmail.com>
2026-03-17 11:09:40 +0000

    Create ChallengersPlayers.js

---
+++ src/data/ChallengersPlayers.js
@@ -0,0 +1,857 @@
+// src/data/challengersPlayers.js
+// Real Challengers seed pool provided by user; deduplicated and filtered to exclude current CDL starters.
+
+export const challengersPlayers = [
+  {
+    "name": "oJohnny",
+    "age": 21,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "Afro",
+    "age": 23,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "KnoX",
+    "age": 22,
+    "role": "AR",
+    "region": "NA"
+  },
+  {
+    "name": "Standy",
+    "age": 24,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "Brack",
+    "age": 25,
+    "role": "AR",
+    "region": "NA"
+  },
+  {
+    "name": "Asim",
+    "age": 24,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "PaulEhx",
+    "age": 23,
+    "role": "AR",
+    "region": "NA"
+  },
+  {
+    "name": "FeLo",
+    "age": 28,
+    "role": "AR",
+    "region": "NA"
+  },
+  {
+    "name": "Arcitys",
+    "age": 27,
+    "role": "AR",
+    "region": "NA"
+  },
+  {
+    "name": "Gunless",
+    "age": 27,
+    "role": "AR",
+    "region": "NA"
+  },
+  {
+    "name": "Vivid",
+    "age": 24,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "Mack",
+    "age": 23,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "Owakening",
+    "age": 24,
+    "role": "AR",
+    "region": "NA"
+  },
+  {
+    "name": "Prolute",
+    "age": 22,
+    "role": "AR",
+    "region": "NA"
+  },
+  {
+    "name": "Fire",
+    "age": 21,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "Diamondcon",
+    "age": 23,
+    "role": "AR",
+    "region": "NA"
+  },
+  {
+    "name": "Beans",
+    "age": 21,
+    "role": "AR",
+    "region": "NA"
+  },
+  {
+    "name": "EriKBooM",
+    "age": 20,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "Manny",
+    "age": 19,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "Dak",
+    "age": 20,
+    "role": "AR",
+    "region": "NA"
+  },
+  {
+    "name": "zvik",
+    "age": 19,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "Wrecks",
+    "age": 22,
+    "role": "AR",
+    "region": "NA"
+  },
+  {
+    "name": "Cimbels",
+    "age": 20,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "Ryann",
+    "age": 21,
+    "role": "AR",
+    "region": "NA"
+  },
+  {
+    "name": "Milly",
+    "age": 20,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "Peel",
+    "age": 19,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "Swooty",
+    "age": 21,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "KingAbody",
+    "age": 19,
+    "role": "AR",
+    "region": "NA"
+  },
+  {
+    "name": "MajorManiak",
+    "age": 27,
+    "role": "AR",
+    "region": "NA"
+  },
+  {
+    "name": "Nemensions",
+    "age": 20,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "Alluka",
+    "age": 19,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "qk4b",
+    "age": 18,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "Flames",
+    "age": 20,
+    "role": "AR",
+    "region": "NA"
+  },
+  {
+    "name": "Gio",
+    "age": 21,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "Uli",
+    "age": 22,
+    "role": "AR",
+    "region": "NA"
+  },
+  {
+    "name": "Nagafen",
+    "age": 29,
+    "role": "AR",
+    "region": "NA"
+  },
+  {
+    "name": "Renegade",
+    "age": 22,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "Rspi",
+    "age": 20,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "JuJu",
+    "age": 23,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "Darksived",
+    "age": 21,
+    "role": "AR",
+    "region": "NA"
+  },
+  {
+    "name": "Phantomz",
+    "age": 25,
+    "role": "AR",
+    "region": "NA"
+  },
+  {
+    "name": "Ruper",
+    "age": 22,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "KB",
+    "age": 19,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "Nate",
+    "age": 20,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "Cruze",
+    "age": 21,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "Gunsiii",
+    "age": 20,
+    "role": "AR",
+    "region": "NA"
+  },
+  {
+    "name": "Mystvc",
+    "age": 18,
+    "role": "SMG",
+    "region": "NA"
+  },
+  {
+    "name": "Lynz",
+    "age": 19,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "Breszy",
+    "age": 25,
+    "role": "AR",
+    "region": "EU"
+  },
+  {
+    "name": "Cobra",
+    "age": 22,
+    "role": "AR",
+    "region": "EU"
+  },
+  {
+    "name": "Melko",
+    "age": 20,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "Harry",
+    "age": 23,
+    "role": "AR",
+    "region": "EU"
+  },
+  {
+    "name": "Denza",
+    "age": 24,
+    "role": "AR",
+    "region": "EU"
+  },
+  {
+    "name": "Aspekt",
+    "age": 21,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "Vortex",
+    "age": 29,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "D7om",
+    "age": 22,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "Khhx",
+    "age": 23,
+    "role": "AR",
+    "region": "EU"
+  },
+  {
+    "name": "Rxld",
+    "age": 21,
+    "role": "AR",
+    "region": "EU"
+  },
+  {
+    "name": "Dany",
+    "age": 20,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "Depaa",
+    "age": 22,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "Nadox",
+    "age": 21,
+    "role": "AR",
+    "region": "EU"
+  },
+  {
+    "name": "SmolZiLa",
+    "age": 19,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "Infinity",
+    "age": 22,
+    "role": "AR",
+    "region": "EU"
+  },
+  {
+    "name": "Syqz",
+    "age": 21,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "Mighty",
+    "age": 23,
+    "role": "AR",
+    "region": "EU"
+  },
+  {
+    "name": "Detain",
+    "age": 22,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "Wailers",
+    "age": 27,
+    "role": "AR",
+    "region": "EU"
+  },
+  {
+    "name": "Twisted",
+    "age": 21,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "Henry",
+    "age": 24,
+    "role": "AR",
+    "region": "EU"
+  },
+  {
+    "name": "Lqgend",
+    "age": 22,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "Ovrlay",
+    "age": 21,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "Rush",
+    "age": 23,
+    "role": "AR",
+    "region": "EU"
+  },
+  {
+    "name": "Kez",
+    "age": 20,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "Lampy",
+    "age": 24,
+    "role": "AR",
+    "region": "EU"
+  },
+  {
+    "name": "Shay",
+    "age": 19,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "Synergy",
+    "age": 21,
+    "role": "AR",
+    "region": "EU"
+  },
+  {
+    "name": "Mythix",
+    "age": 20,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "FDot",
+    "age": 25,
+    "role": "AR",
+    "region": "EU"
+  },
+  {
+    "name": "Avo",
+    "age": 20,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "Izaac",
+    "age": 21,
+    "role": "AR",
+    "region": "EU"
+  },
+  {
+    "name": "Teslu",
+    "age": 22,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "Sway",
+    "age": 19,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "Komo",
+    "age": 23,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "Chunk",
+    "age": 20,
+    "role": "AR",
+    "region": "EU"
+  },
+  {
+    "name": "Scythe",
+    "age": 21,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "GViio",
+    "age": 19,
+    "role": "AR",
+    "region": "EU"
+  },
+  {
+    "name": "RealCharm",
+    "age": 20,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "RaZeSouLx",
+    "age": 21,
+    "role": "AR",
+    "region": "EU"
+  },
+  {
+    "name": "d7oomx",
+    "age": 22,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "DarkHorse",
+    "age": 20,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "Frenchie",
+    "age": 23,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "Lucky",
+    "age": 26,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "Vikul",
+    "age": 22,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "Eriksan",
+    "age": 24,
+    "role": "AR",
+    "region": "EU"
+  },
+  {
+    "name": "WarDy",
+    "age": 23,
+    "role": "SMG",
+    "region": "EU"
+  },
+  {
+    "name": "Rise",
+    "age": 19,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "Pegg",
+    "age": 20,
+    "role": "AR",
+    "region": "MENA"
+  },
+  {
+    "name": "Faisal",
+    "age": 23,
+    "role": "AR",
+    "region": "MENA"
+  },
+  {
+    "name": "9JamaL",
+    "age": 21,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "ZiddiX",
+    "age": 20,
+    "role": "AR",
+    "region": "MENA"
+  },
+  {
+    "name": "Aiden",
+    "age": 22,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "CleemZi",
+    "age": 20,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "Ice",
+    "age": 21,
+    "role": "AR",
+    "region": "MENA"
+  },
+  {
+    "name": "Only",
+    "age": 19,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "GntL",
+    "age": 20,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "D3L1V3R",
+    "age": 24,
+    "role": "AR",
+    "region": "MENA"
+  },
+  {
+    "name": "Alfa",
+    "age": 21,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "Xotic",
+    "age": 26,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "Majed",
+    "age": 22,
+    "role": "AR",
+    "region": "MENA"
+  },
+  {
+    "name": "Yazeed",
+    "age": 20,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "Sult",
+    "age": 21,
+    "role": "AR",
+    "region": "MENA"
+  },
+  {
+    "name": "Rakan",
+    "age": 19,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "Hitmxn",
+    "age": 21,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "Mince",
+    "age": 20,
+    "role": "AR",
+    "region": "MENA"
+  },
+  {
+    "name": "Tkay",
+    "age": 19,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "Cxnfid",
+    "age": 18,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "Couti",
+    "age": 21,
+    "role": "AR",
+    "region": "MENA"
+  },
+  {
+    "name": "ZooNer",
+    "age": 22,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "cWadz",
+    "age": 20,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "GloFrosty",
+    "age": 26,
+    "role": "AR",
+    "region": "MENA"
+  },
+  {
+    "name": "Servyr",
+    "age": 19,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "Booger",
+    "age": 20,
+    "role": "AR",
+    "region": "MENA"
+  },
+  {
+    "name": "Cryptic",
+    "age": 23,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "Andi",
+    "age": 21,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "Radial",
+    "age": 22,
+    "role": "AR",
+    "region": "MENA"
+  },
+  {
+    "name": "Noysii",
+    "age": 24,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "Fame",
+    "age": 24,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "Leib",
+    "age": 21,
+    "role": "AR",
+    "region": "MENA"
+  },
+  {
+    "name": "Mirage",
+    "age": 20,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "Prodahh",
+    "age": 19,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "Jett",
+    "age": 22,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "Husk",
+    "age": 21,
+    "role": "AR",
+    "region": "MENA"
+  },
+  {
+    "name": "VinnyReaper",
+    "age": 23,
+    "role": "AR",
+    "region": "MENA"
+  },
+  {
+    "name": "Prod",
+    "age": 22,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "Derek",
+    "age": 20,
+    "role": "AR",
+    "region": "MENA"
+  },
+  {
+    "name": "Swagchecked",
+    "age": 19,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "Dessie",
+    "age": 18,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "Calamity",
+    "age": 21,
+    "role": "AR",
+    "region": "MENA"
+  },
+  {
+    "name": "Conwa",
+    "age": 20,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "Sizkie",
+    "age": 22,
+    "role": "AR",
+    "region": "MENA"
+  },
+  {
+    "name": "Crazie",
+    "age": 19,
+    "role": "SMG",
+    "region": "MENA"
+  },
+  {
+    "name": "Choco",
+    "age": 21,
+    "role": "AR",
+    "region": "MENA"
+  }
+];
